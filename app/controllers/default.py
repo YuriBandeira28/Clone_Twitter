2d0e86d9f4917ed974f92bdfbdf9ce7e2443356d
YuriBandeira28 <yuri.2021002726@aluno.iffar.edu.br>
2023-05-17 15:40:37 -0300

    alterações básicas controllers
    rotas e exemplos

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,17 @@
 from app import app
 
-@app.route("/")
+@app.route("/") # <- decorator
 def index():
     return "Hello world"
+
+@app.route("/test", defaults={'name': None})
+@app.route("/test/<name>")
+def test(name):
+    if name:
+        return 'Olá, %s!' % name
+    else:
+        return "Olá"
+    
+@app.route("/inteiro/<int:num>") # força tipagem
+def interito(num):
+    return f"numero é {num}"
